0e7cb42261b6ba41a11a085aeeadaa080eede566
kalosisz <kalosi.szilard@gmail.com>
2020-10-25 21:57:31 +0100

    Multi-worker TF training example on AI platform

---
+++ multi-woker/trainer/task.py
@@ -0,0 +1,66 @@
+import os
+import json
+from math import ceil
+from argparse import ArgumentParser
+
+import tensorflow as tf
+
+from trainer.data import (
+    numeric_feature_descriptions, get_dataset, get_steps_per_epoch)
+from trainer.model import create_model
+from trainer.utils import save_model
+
+
+def main(args):
+    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
+    with strategy.scope():
+        multi_worker_model = create_model(
+            numeric_feature_descriptions.keys(), 
+            args.layer_sizes, 
+            'mse',
+            tf.keras.optimizers.Adam(args.learning_rate)
+        )
+
+    training_dataset = get_dataset(
+        os.path.join(args.data_base_path, 'train/*.tfrecord'),
+        strategy,
+        args.train_batch_size,
+        True)
+
+    validation_dataset = get_dataset(
+        os.path.join(args.data_base_path, 'val/*.tfrecord'),
+        strategy,
+        args.validation_batch_size,
+        False)
+
+    steps_per_epoch = get_steps_per_epoch(
+        strategy, args.training_examples, args.train_batch_size)
+    validation_steps = get_steps_per_epoch(
+        strategy, args.validation_examples, args.train_batch_size)
+
+    multi_worker_model.fit(
+        training_dataset,
+        epochs=args.epochs,
+        steps_per_epoch=steps_per_epoch,
+        validation_data=validation_dataset,
+        validation_steps=validation_steps
+    )
+
+    save_model(os.path.join(args.job_dir, 'saved_model'), multi_worker_model)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("--job-dir", required=True)
+    parser.add_argument("--layer-sizes", nargs="+", type=int, required=True)
+    parser.add_argument("--learning-rate", type=float, required=True)
+    parser.add_argument('--epochs',type=int, required=True)
+    parser.add_argument("--data-base-path",required=True)
+    parser.add_argument('--training-examples', type=int, required=True)
+    parser.add_argument('--validation-examples', type=int, required=True)
+    parser.add_argument('--train-batch-size', type=int, default=1000)
+    parser.add_argument('--validation-batch-size', type=int, default=10000)
+
+    main(parser.parse_args())
---
+++ multi-woker/trainer/utils.py
@@ -0,0 +1,35 @@
+import os
+from time import sleep
+import tensorflow as tf
+
+
+def _is_chief(cluster_resolver):
+    task_type = cluster_resolver.task_type
+    return  task_type is None or task_type == 'chief'
+
+
+def _get_temp_dir(model_path, cluster_resolver):
+  worker_temp = f'worker{cluster_resolver.task_id}_temp'
+  return os.path.join(model_path, worker_temp)
+
+
+def save_model(model_path, model):
+    # the following is need for TF 2.2. 2.3 onward, it can be accessed from strategy
+    cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver()
+    is_chief = _is_chief(cluster_resolver)
+    
+    print(cluster_resolver)
+    print(not is_chief)
+    if not is_chief:
+        model_path = _get_temp_dir(model_path, cluster_resolver)
+
+    model.save(model_path)
+
+    if is_chief:
+        # wait for workers to delete; check every 100ms
+        # if chief is finished, the training is done
+        while tf.io.gfile.glob(os.path.join(model_path, "worker*")):
+            sleep(0.1)
+
+    if not is_chief:
+        tf.io.gfile.rmtree(model_path)
